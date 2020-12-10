d2f4fe611fe4ed47d1e8d4502eaad443c7a7f256
= <=>
2020-12-10 14:17:55 -0600

    Agregadas las Variables Personalizadas de Heroku

--- server/config/config.js
+++ server/config/config.js
@@ -14,12 +14,11 @@ process.env.NODE_ENV = process.env.PORT || 'dev';
 /// BD
 /// =================================
 
-// let urlDB;
-// if (process.env.NODE_ENV === 'dev') {
-// urlDB = 'mongodb://localhost:27017/cafe';
-// } else {
-// 
-urlDB = 'mongodb://franco:rGDB8fR3roWMMbGq@cluster0.rnnbi.mongodb.net/cafe';
-// }
-// 
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = process.env.MONGO_URI;
+}
+
 process.env.URLDB = urlDB;
